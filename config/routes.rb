58a4b929dab2ecf89c5a25f0278294568717dfa5
takumi20210430 <uniquetime.t@gmail.com>
2021-05-17 08:29:36 +0000

    [add model,controller作成]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,38 @@
 Rails.application.routes.draw do
+  
+  devise_for :customers
+  namespace :public do
+    root 'homes#top'
+     get 'home/about' => 'homes#about'
+    resources :items, only: [:index, :show]
+    resource :customers, except: [:index, :new, :create, :destroy]
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+    resources :orders, only: [:new, :create, :index, :show]
+    post 'orders/confilm' => 'orders#confilm'
+    get 'orders/complete' => 'orders#complete'
+    resources :addresses, except: [:new, :show]
+    resources :cart_items, except: [:new, :show, :edit]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+
+  end
+
+
+  devise_for :admins
+  namespace :admin do
+    root 'homes#top'
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, except: [:new, :create, :destroy]
+    get 'orders/:id' => 'orders#show'
+    patch 'orders/:id' => 'orders#update' do
+      patch 'order_details/:id' => 'order_details#update'
+    end
+
+  end
+
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
---
+++ db/migrate/20210517073845_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id
+      t.string :shipping_zip_code
+      t.string :shipping_address
+      t.string :shipping_name
+      t.integer :delivery_fee
+      t.integer :total_price
+      t.integer :method_of_payment
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
