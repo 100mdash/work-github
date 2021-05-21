ce21ecb0cff67610753854aed23a8c3d6c495791
takumi20210430 <uniquetime.t@gmail.com>
2021-05-21 05:32:33 +0000

    途中経過

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+  has_many :order_details, dependent: :destroy
+  belongs_to :customer
+
+  def delivery_fee
+  end
+
+ enum method_of_payment:{クレジットカード: 0, 銀行振込: 1}
+
 end
