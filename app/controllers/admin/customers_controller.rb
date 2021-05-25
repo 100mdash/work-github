734d3a31e407a6729c1a8996e65ab3839bcdc9b1
takumi20210430 <uniquetime.t@gmail.com>
2021-05-25 10:36:12 +0000

    kansei

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @customers = Customer.all
-    @prducts = Customer.page(params[:page]).per(1)
+    @customers = Customer.page(params[:page])
   end
 
   def show
@@ -18,7 +17,7 @@ class Admin::CustomersController < ApplicationController
     @customer.update(customer_params)
     redirect_to admin_customer_path(@customer)
   end
-  
+
 private
 
   def customer_params
